54f93fe1c4b0a6075983d65b21bbf70fc0f71c5f
Emma971 <57229866+Emma971@users.noreply.github.com>
2020-04-06 09:10:26 +0200

    Make it a project

---
+++ src/model/Klas.java
@@ -0,0 +1,19 @@
+package src.model;
+
+public class Klas {
+	private String naam;
+	
+	protected Klas(String nm) {
+		naam = nm;
+	}
+	
+	public String getNaam() {
+		return naam;
+	}
+
+	public boolean equals(Object obj) {
+		boolean gelijk = obj instanceof Klas;
+		gelijk = gelijk && ((Klas)obj).naam.equals(naam);
+		return gelijk;
+	}
+}
